657151d24c100a0895b4a049270887dc0b59e3d4
Saad T <tariqs26@mcmaster.ca>
2022-07-15 17:07:44 -0400

    reformat code, chain multiple appends

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // function to simulate loading
 const addDelay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const form = document.querySelector("form");
 const input = form.children[0];
 form.addEventListener("submit", async (evt) => {
@@ -18,6 +17,7 @@ form.addEventListener("submit", async (evt) => {
   await addDelay(1500);
   const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
   document.querySelector(".loading").classList.add("hidden");
+
   // no results
   if (!res.data.length) {
     document.querySelector(".image").innerHTML = "<h1>No results found</h1>";
@@ -27,23 +27,14 @@ form.addEventListener("submit", async (evt) => {
   for (let show of res.data) {
     const movie = document.createElement("div");
     movie.classList.add("movie");
-
     const newImg = document.createElement("img");
     newImg.src =
       show.show.image && show.show.image.medium
         ? show.show.image && show.show.image.medium
         : "https://via.placeholder.com/220x290";
-
     const title = document.createElement("div");
     title.innerText = show.show.name;
-
-    const rating = document.createElement("span");
-    rating.classList.add("rating");
-    rating.innerText = show.show.rating.average || "N/A";
-
-    movie.append(newImg);
-    movie.append(title);
-
+    movie.append(newImg, title);
     document.querySelector(".image").append(movie);
   }
 });
